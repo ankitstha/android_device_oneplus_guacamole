eb4542b671efb68afc89cc173346a4d5b3601b87
Ankit Shrestha <ankitshrestha114@gmail.com>
2024-03-17 08:01:49 +0545

    Horizonify

--- aosp_guacamole.mk
+++ aosp_guacamole.mk
@@ -11,15 +11,24 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from guacamole device
 $(call inherit-product, device/oneplus/guacamole/device.mk)
 
-# Inherit some common YAAP stuff.
-$(call inherit-product, vendor/yaap/config/common_full_phone.mk)
+# Inherit some common HorizonDroid stuff.
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := yaap_guacamole
+PRODUCT_NAME := aosp_guacamole
 PRODUCT_DEVICE := guacamole
 PRODUCT_BRAND := OnePlus
 PRODUCT_MODEL := GM1917
 PRODUCT_MANUFACTURER := OnePlus
+
+# HorizonDroid prop
+HORIZON_MAINTAINER := c00chiemilker
+HORIZON_BUILD_TYPE := UNOFFICIAL
+TARGET_BOOT_ANIMATION_RES := 1080
+
+* Optional 
+TARGET_BUILD_VIMUSIC := false
+TARGET_BUILD_INNERTUNE := true
 
 PRODUCT_SYSTEM_NAME := OnePlus7Pro
 PRODUCT_SYSTEM_DEVICE := OnePlus7Pro
@@ -27,11 +36,6 @@ PRODUCT_SYSTEM_DEVICE := OnePlus7Pro
 PRODUCT_AAPT_CONFIG := xxxhdpi
 PRODUCT_AAPT_PREF_CONFIG := xxxhdpi
 PRODUCT_CHARACTERISTICS := nosdcard
-
-# Boot animation
-scr_resolution := 1440
-TARGET_SCREEN_HEIGHT := 3120
-TARGET_SCREEN_WIDTH := 1440
 
 # Build info
 BUILD_FINGERPRINT := "OnePlus/OnePlus7Pro/OnePlus7Pro:11/RKQ1.201022.002/2206171030:user/release-keys"
