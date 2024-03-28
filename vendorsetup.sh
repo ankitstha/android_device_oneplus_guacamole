09749b6968d114d1c116caee14b4f8ebf1f5589f
ankitstha <ankitshrestha114@gmail.com>
2024-03-28 20:15:26 +0545

    Add vendorsetup.sh

---
+++ vendorsetup.sh
@@ -0,0 +1,4 @@
+git clone https://github.com/DerpFest-Devices/device_oneplus_sm8150-common.git -b 13 device/oneplus/sm8150-common
+git clone https://github.com/DerpFest-Devices/vendor_oneplus.git -b 13 vendor/oneplus
+git clone https://github.com/DerpFest-Devices/kernel_oneplus_sm8150.git -b 13 kernel/oneplus/sm8150 --depth=1
+git clone https://github.com/DerpFest-Devices/hardware_oneplus.git -b 13 hardware/oneplus
